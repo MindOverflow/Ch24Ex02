ff33a9edf331a7875faa0d3a995673b68e743c14
MindOverflow <toxaSmith@yahoo.com>
2017-03-24 16:31:26 +0300

    [add] Initial commit.

---
+++ Ch24Ex02/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch24Ex02
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            WriteLine("Основной поток выполнения запущен.");
+
+            var mc = new MyClass();
+
+            var task = new Task(mc.MyTask);
+
+            task.Start();
+
+            for (int i = 0; i < 60; i++)
+            {
+                Write(".");
+                Thread.Sleep(100);
+            }
+
+            WriteLine("Основной поток выполнения завершён.");
+        }
+    }
+
+    internal class MyClass
+    {
+        // Метод выполняющийся в качестве задачи.
+        public void MyTask()
+        {
+            WriteLine("MyTask запущен.");
+
+            for (var count = 0; count < 10; count++)
+            {
+                Thread.Sleep(500);
+                WriteLine($"В методе MyTask(), подсчёт равен {count}");
+            }
+
+            WriteLine("MyTask pзавершён.");
+        }
+    }
+}
